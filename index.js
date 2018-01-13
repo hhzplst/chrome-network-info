a89654ab41efd9d93619109d96c391810c39e07e
Ivy Chang <ivycwq@icloud.com>
2018-01-13 09:27:10 -0800

    fixing ports

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8000;
+
+const CDP = require('chrome-remote-interface');
 
 app.get('/', function(req, res) {
-
+  res.send("hello");
 });
 
-app.listen(8000, function() {
-  console.log("server's running on port 8000");
+app.listen(port, function() {
+  console.log("server's running on port " + port);
 });
